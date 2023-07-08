71c2f8a2d1e833fe11438e733dd891b0c5f26a2b
sebastian <sespinozahenriquez@gmail.com>
2023-07-08 12:09:16 -0400

    ajustes

--- src/html/Login.js
+++ src/html/Login.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 //import fuera de react
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '../css/Card.css';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -32,24 +33,28 @@ const Login = () => {
     return (
         <>
             <ToastContainer />
-            <div class="card">
-                <div class="card-header">
-                    Featured
-                </div>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Usuario</label>
-                            <input type="user" class="form-control" id="usuario" aria-describedby="emailHelp" placeholder="Enter email" />
-                            <small id="emailHelp" class="form-text text-muted">Intente con Usuario.</small>
+            <div className="card-container">
+                <div class="card col-xs-12 col-md-6 col-lg-6 border-success">
+                    <div class="card-header text-white" style={{backgroundColor: '#138138' }}>
+                        Bienvenido
+                    </div>
+                    <div class="card-body" style={{backgroundColor: '#139139' }}>
+                    <div class="d-flex justify-content-center">
+                        <form>
+                            <div class="form-group text-white mt-2">
+                                <label for="exampleInputEmail1">Usuario</label>
+                                <input type="user" class="form-control" id="usuario" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <small id="emailHelp" class="form-text text-black">Intente con Usuario.</small>
+                            </div>
+                            <div class="form-group text-white mt-2">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="pass" placeholder="Password" />
+                                <small id="passwordHelp" class="form-text text-black">Intente con 123.</small>
+                            </div>
+                            <button type="button" class="btn btn-light mt-2" onClick={login}>Submit</button>
+                        </form>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="pass" placeholder="Password" />
-                            <small id="passwordHelp" class="form-text text-muted">Intente con 123.</small>
-                        </div>
-                        <button type="button" class="btn btn-primary" onClick={login}>Submit</button>
-                    </form>
+                    </div>
                 </div>
             </div>
 
